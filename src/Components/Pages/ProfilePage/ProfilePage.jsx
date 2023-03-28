80a334f71bad1b098e3d1c7fb794a242af8f3bbf
ShleeRE <konradbralewski1111@gmail.com>
2023-03-28 18:44:39 +0200

    ProfilePage

--- src/Components/Pages/ProfilePage/ProfilePage.jsx
+++ src/Components/Pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,11 @@
+import profilePic from "Assets/profilePicture.jpg"
+
 export default function ProfilePage(){
     return (
-        <div>
-            <p>pp</p>
-        </div>
+        <main>
+            <p className="m-auto text-sm tablet:text-base laptop:text-xl text-center">acc name</p>
+            <img src={profilePic} alt="Your profile" className="object-cover my-2 m-auto h-36 tablet:h-60 laptop:h-72"/>
+            {<p>your posts</p>}
+        </main>
     )
 }
